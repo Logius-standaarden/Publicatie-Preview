358115b8f4e0f40f961080719003327813b0417c
GitHub Action <api@logius.nl>
2022-04-29 14:51:20 +0000

    new preview build

---
+++ OAuth-NL-profiel/examples/js/config.js
@@ -0,0 +1,53 @@
+var respecConfig = {
+  specStatus: "GN-DEF",
+  specType: "HR",
+  pubDomain: "etc",
+  publishDate: "2017-12-13",
+  editors: [
+    {
+      name: "Frank Terpstra",
+      company: "Geonovum",
+      companyURL: "http://www.geonovum.nl/",
+    },
+    {
+      name: "auteur 2",
+      company: "organisatie",
+      companyURL: "http://www.organisatie.nl/",
+    },
+  ],
+  shortName: "Nederlands profiel OAuth 2.0",
+  subtitle: "Werkversie",
+  github: "https://github.com/Logius-standaarden/KP-APIs-OAuthNL",
+  issueBase: "https://github.com/Logius-standaarden/KP-APIs-OAuthNL/issues/",
+  alternateFormats: [
+    {
+      label: "pdf",
+      uri: "OAuth-NL-profiel.pdf",
+    },
+    ],
+  localBiblio: {
+    "OAuth2": {
+      href: "https://tools.ietf.org/html/rfc6749",
+      title:
+        "The OAuth 2.0 Authorization Framework",
+      authors: ["D. Hardt"],
+      date: "October 2012",
+      publisher: "The Internet Engineering Task Force",
+    },
+	"Expert": {
+      href: "https://www.forumstandaardisatie.nl/sites/bfs/files/Expertadvies%20OAuth%202.0.pdf",
+      title:
+        "Expertadvies OAuth 2.0",
+      authors: ["P. Dam"],
+      date: "24 februari 2017",
+      publisher: "Forum STandaardisatie",
+    },
+    "UUID": {
+      href:  'https://tools.ietf.org/html/rfc4122',
+      title: 'A Universally Unique IDentifier (UUID) URN Namespace',
+      authors: ['P. Leach', 'M. Mealling', 'R. Salz'],
+      date: 'July 2005',
+      publisher: 'The Internet Engineering Task Force'
+    }
+  },
+};
