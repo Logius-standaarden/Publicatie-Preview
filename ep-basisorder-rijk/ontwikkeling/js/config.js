b43f4168a23042388fb1725e5991eef138e4e20b
GitHub Action <api@logius.nl>
2022-11-29 13:39:45 +0000

    new preview build

---
+++ ep-basisorder-rijk/ontwikkeling/js/config.js
@@ -0,0 +1,61 @@
+var respecConfig = {
+    //voor specStatus mogelijkheden zie https://github.com/Logius-standaarden/respec/wiki/specStatus
+    specStatus: "WV",
+    //voor specType mogelijkheden zie https://github.com/Logius-standaarden/respec/wiki/specType
+    specType: "HR",
+    pubDomain: "ep",
+    shortName: "basisorder-rijk",
+    publishDate: "2022-11-29",
+    publishVersion: "0.7",
+    title: "Handreiking Basisorder Rijk",
+    // previousPublishVersion: "(none)",
+    // previousPublishDate: "(none)",
+    // previousMaturity: "WV",
+    content:
+        {
+            "handreiking-basisorderrijk": "",
+            "overzicht-elementen-basisorderrijk": "",
+        },
+    editors:
+        [
+            {
+                name: "HR",
+                company: "Logius",
+                companyURL: "https://www.logius.nl",
+            },
+            {
+                name: "MvB",
+                company: "Logius",
+                companyURL: "https://www.logius.nl",
+            },
+        ],
+    authors:
+        [
+            {
+                name: "Logius Standaarden - team e-procurement",
+                company: "Logius",
+                companyURL: "https://logius.nl/diensten/e-procurement",
+            }
+        ],
+    github: "https://github.com/Logius-standaarden/ep-basisorder-rijk",
+
+
+    // Controls if linked "§" section markers are added to a document
+    addSectionLinks: true,
+
+    // this parameter will add the tag_name of the latest release to the document Title
+    // only set this parameter when a release has been set
+    nl_addReleaseTagTitle: false,
+
+    // if nl_markdownEmbedImageInFigure is set to true images in markdown generated content will be surrounded with <figures> element
+    // so that figures can be linked are be a part of table of figures
+    nl_markdownEmbedImageInFigure: true,
+
+    // Create PDF and link to file in header:
+    // alternateFormats: [
+    //     {
+    //         label: "pdf",
+    //         uri: "rename-me.pdf",
+    //     },
+    // ],
+};
