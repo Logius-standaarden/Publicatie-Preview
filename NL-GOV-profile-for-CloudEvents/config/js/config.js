5b2b1e1184fb9b7179f18429c0411253e5bfbfaf
GitHub Action <api@logius.nl>
2023-03-14 16:29:30 +0000

    new preview build

---
+++ NL-GOV-profile-for-CloudEvents/config/js/config.js
@@ -0,0 +1,39 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "CloudEvents-NL.pdf"
+      } ],
+  authors: [ { 
+        "company" : "Project Notificatieservices",
+        "name" : "Werkgroep Berichtenstandaard"
+      } ],
+  editors: [ 
+      { 
+        "company" : "VNG Realisatie",
+        "companyURL" : "https://www.vngrealisatie.nl/",
+        "name" : "Gershon Jansen"
+      },
+      { 
+        "company" : "VNG Realisatie",
+        "companyURL" : "https://www.vngrealisatie.nl/",
+        "name" : "Ad Gerrits"
+      },
+      { 
+        "company" : "Logius",
+        "name" : "Edwin Wisse",
+        "url" : "https://logius.nl/standaarden"
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/NL-GOV-profile-for-CloudEvents",
+  issueBase: "https://github.com/Logius-standaarden/NL-GOV-profile-for-CloudEvents/issues/",
+  maxTocLevel: 3,
+  nl_markdownCodeClass: "code",
+  nl_markdownTableClass: "dkkvs",
+  pubDomain: "notificatieservices",
+  publishDate: "2022-07-05",
+  publishVersion: "0.3",
+  shortName: "CloudEvents-NL",
+  specStatus: "WD",
+  specType: "HR",
+  subtitle: "Project Notificatieservices"
+};
