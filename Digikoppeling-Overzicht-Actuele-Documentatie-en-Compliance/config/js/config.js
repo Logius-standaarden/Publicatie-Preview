ff2f8cbb0eb2d5782165e212cbd592c536c7ada3
GitHub Action <api@logius.nl>
2023-03-14 14:03:03 +0000

    new preview build

---
+++ Digikoppeling-Overzicht-Actuele-Documentatie-en-Compliance/config/js/config.js
@@ -0,0 +1,31 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "Digikoppeling-Overzicht-Actuele-Documentatie-en-Compliance.pdf"
+      } ],
+  authors: [ { 
+        "name" : "Pieter Hering",
+        "url" : "https://logius.nl/standaarden"
+      } ],
+  editors: [ 
+      { 
+        "name" : "Peter Haasnoot",
+        "url" : "https://logius.nl/standaarden"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://logius.nl/standaarden",
+        "mailto" : "api@digikoppeling.nl",
+        "name" : "Pieter Hering"
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/Digikoppeling-Overzicht-Actuele-Documentatie-en-Compliance",
+  previousPublishDate: "2022-08-12",
+  previousPublishVersion: "1.9.1",
+  pubDomain: "dk",
+  publishDate: "2022-11-14",
+  publishVersion: "1.10",
+  shortName: "actueel",
+  specStatus: "DEF",
+  specType: "ST"
+};
