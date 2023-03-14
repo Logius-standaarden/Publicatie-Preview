2fa155691b0a576ea09044bdf60e0ed5b170bf00
GitHub Action <api@logius.nl>
2023-03-14 16:28:07 +0000

    new preview build

---
+++ API-Design-Rules/config/js/config.js
@@ -0,0 +1,47 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "API-Design-Rules.pdf"
+      } ],
+  authors: [ 
+      { 
+        "company" : "Het Kadaster",
+        "companyURL" : "https://www.kadaster.nl",
+        "name" : "Jasper Roes"
+      },
+      { 
+        "company" : "Het Kadaster",
+        "companyURL" : "https://www.kadaster.nl",
+        "name" : "Joost Farla"
+      }
+    ],
+  editors: [ 
+      { 
+        "company" : "Geonovum",
+        "companyURL" : "https://www.geonovum.nl",
+        "name" : "Frank Terpstra"
+      },
+      { 
+        "company" : "Geonovum",
+        "companyURL" : "https://www.geonovum.nl",
+        "name" : "Jan van Gelder"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://www.logius.nl",
+        "name" : "Alexander Green"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://www.logius.nl",
+        "name" : "Martin van der Plas"
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/API-Design-Rules",
+  pubDomain: "api",
+  publishDate: "2022-01-25",
+  publishVersion: "1.1",
+  shortName: "adr",
+  specStatus: "WV",
+  specType: "ST"
+};
