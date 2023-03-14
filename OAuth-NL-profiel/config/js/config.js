1cbf49168cf92804690d53bfa298e24e36909cde
GitHub Action <api@logius.nl>
2023-03-14 16:25:54 +0000

    new preview build

---
+++ OAuth-NL-profiel/config/js/config.js
@@ -0,0 +1,83 @@
+var respecConfig = {
+  authors: [ 
+      { 
+        "company" : "Logius",
+        "companyURL" : "http://www.logius.nl/",
+        "name" : "Jaron Azaria"
+      },
+      { 
+        "company" : "Kadaster",
+        "companyURL" : "http://www.kadaster.nl/",
+        "name" : "Martin Borgman"
+      },
+      { 
+        "company" : "Kennisnet",
+        "companyURL" : "http://www.kennisnet.nl/",
+        "name" : "Marc Fleischeuers"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "http://www.logius.nl/",
+        "name" : "Peter Haasnoot"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "http://www.logius.nl/",
+        "name" : "Leon van der Ree"
+      },
+      { 
+        "company" : "RvIG",
+        "companyURL" : "http://www.rvig.nl/",
+        "name" : "Bob te Riele"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "http://www.logius.nl/",
+        "name" : "Remco Schaar"
+      },
+      { 
+        "company" : "Geonovum",
+        "companyURL" : "http://geonovum.nl/",
+        "name" : "Frank Terpstra"
+      },
+      { 
+        "company" : "Rijkswaterstaat",
+        "companyURL" : "http://www.rws.nl/",
+        "name" : "Jan Jaap Zoutendijk"
+      }
+    ],
+  editors: [ 
+      { 
+        "company" : "Geonovum",
+        "companyURL" : "http://www.geonovum.nl/",
+        "name" : "Frank Terpstra"
+      },
+      { 
+        "company" : "Geonovum",
+        "companyURL" : "http://www.geonovum.nl/",
+        "name" : "Jan van Gelder"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "http://www.logius.nl/",
+        "name" : "Alexander Green"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "http://www.logius.nl/",
+        "mailto" : "api@logius.nl",
+        "name" : "Martin van der Plas"
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/OAuth-NL-profiel/",
+  issueBase: "https://github.com/Logius-standaarden/OAuth-NL-profiel/issues",
+  licence: "cc-by-nd",
+  previousPublishDate: "2020-07-09",
+  previousPublisversion: "1.0",
+  pubDomain: "api",
+  publishDate: "2022-06-17",
+  publishVersion: "v1.1",
+  shortName: "oauth",
+  specStatus: "WV",
+  specType: "ST"
+};
