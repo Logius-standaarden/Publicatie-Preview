12850d3ffdf2148da4567bf9084b72c195d1aa5c
GitHub Action <api@logius.nl>
2023-03-14 16:28:18 +0000

    new preview build

---
+++ Digikoppeling-Koppelvlakstandaard-REST-API/config/js/config.js
@@ -0,0 +1,32 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "Digikoppeling-Koppelvlakstandaard-REST-API.pdf"
+      } ],
+  authors: [ 
+      { 
+        "company" : "Logius",
+        "companyURL" : "http://www.logius.nl/",
+        "name" : "Peter Haasnoot"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "http://www.logius.nl/",
+        "name" : "Pieter Hering"
+      }
+    ],
+  editors: [ { 
+        "company" : "Logius standaardenbeheer",
+        "companyURL" : "http://www.logius.nl/",
+        "name" : ""
+      } ],
+  github: "https://github.com/Logius-standaarden/Digikoppeling-Koppelvlakstandaard-REST-API",
+  nl_emailcomments: "digikoppeling@logius.nl",
+  previousPublishVersion: "1.1.0",
+  pubDomain: "dk",
+  publishDate: "2022-11-14",
+  publishVersion: "1.1.1",
+  shortName: "restapi",
+  specStatus: "WV",
+  specType: "ST"
+};
