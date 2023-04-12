df1c79fa886b7d685e3a8127420192b8c60daae6
GitHub Action <api@logius.nl>
2023-04-12 12:30:23 +0000

    new preview build

---
+++ Logius-Beheermodel/governance-advies/js/config.js
@@ -0,0 +1,27 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "Logius-Beheermodel.pdf"
+      } ],
+  authors: [ { 
+        "mailto" : "bomos@logius.nl",
+        "name" : "Logius",
+        "url" : "https://logius.nl/standaarden"
+      } ],
+  editors: [ { 
+        "company" : "Logius",
+        "name" : "Edwin Wisse",
+        "url" : "https://logius.nl/standaarden"
+      } ],
+  github: "https://github.com/Logius-standaarden/Logius-Beheermodel",
+  nl_markdownCodeClass: "code",
+  nl_markdownTableClass: "dkkvs",
+  previousPublishDate: "2020-12-31",
+  previousPublishVersion: "0.1",
+  pubDomain: "bomos",
+  publishDate: "2022-06-01",
+  publishVersion: "1.0",
+  shortName: "template",
+  specStatus: "WV",
+  specType: "HR"
+};
