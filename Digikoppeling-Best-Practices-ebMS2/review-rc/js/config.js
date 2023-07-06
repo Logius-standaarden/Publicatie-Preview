b34187037de33ca402408a8b06bde90848b3beae
GitHub Action <api@logius.nl>
2023-07-06 14:51:21 +0000

    new preview build

---
+++ Digikoppeling-Best-Practices-ebMS2/review-rc/js/config.js
@@ -0,0 +1,37 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "Digikoppeling-Best-Practices-ebMS2.pdf"
+      } ],
+  authors: [ { 
+        "mailto" : "digikoppeling@logius.nl",
+        "name" : "Logius"
+      } ],
+  editors: [ 
+      { 
+        "company" : "Logius",
+        "name" : "Peter Haasnoot",
+        "url" : "https://logius.nl/standaarden"
+      },
+      { 
+        "company" : "Logius",
+        "name" : "Pieter Hering",
+        "url" : "https://logius.nl/standaarden"
+      },
+      { 
+        "company" : "Logius",
+        "name" : "Martin van der Plas",
+        "url" : "https://logius.nl/standaarden"
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/Digikoppeling-Best-Practices-ebMS2",
+  previousMaturity: "DEF",
+  previousPublishDate: "2022-04-11",
+  previousPublishVersion: "3.2.1",
+  pubDomain: "dk",
+  publishDate: "2023-07-07",
+  publishVersion: "3.2.2",
+  shortName: "bpebms",
+  specStatus: "DEF",
+  specType: "BP"
+};
