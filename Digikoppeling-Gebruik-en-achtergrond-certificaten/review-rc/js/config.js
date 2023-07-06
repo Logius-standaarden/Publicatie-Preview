f714879927f1c9341d2a35e42d64c6468145c700
GitHub Action <api@logius.nl>
2023-07-06 15:12:59 +0000

    new preview build

---
+++ Digikoppeling-Gebruik-en-achtergrond-certificaten/review-rc/js/config.js
@@ -0,0 +1,31 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "Digikoppeling-Gebruik-en-achtergrond-certificaten.pdf"
+      } ],
+  authors: [ { 
+        "mailto" : "digikoppeling@logius.nl",
+        "name" : "Logius"
+      } ],
+  editors: [ 
+      { 
+        "company" : "Logius",
+        "name" : "Peter Haasnoot",
+        "url" : "https://logius.nl/standaarden"
+      },
+      { 
+        "company" : "Logius",
+        "name" : "Pieter Hering",
+        "url" : "https://logius.nl/standaarden"
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/Digikoppeling-Gebruik-en-achtergrond-certificaten",
+  previousPublishDate: "2022-04-11",
+  previousPublishVersion: "1.6.2",
+  pubDomain: "dk",
+  publishDate: "2023-07-07",
+  publishVersion: "1.6.3",
+  shortName: "gbachtcert",
+  specStatus: "DEF",
+  specType: "BP"
+};
