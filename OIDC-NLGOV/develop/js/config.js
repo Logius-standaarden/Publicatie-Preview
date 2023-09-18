4e7d56a30640927ec4d727be224438d5743aa040
GitHub Action <api@logius.nl>
2023-09-18 12:17:09 +0000

    new preview build

---
+++ OIDC-NLGOV/develop/js/config.js
@@ -0,0 +1,73 @@
+let respecConfig = {
+  useLogo: true,
+  useLabel: true,
+  license: "cc-by",
+  specStatus: "DEF",
+  specType: "HR",
+  pubDomain: "api",
+  shortName: "oidc",
+  publishDate: "2023-09-15",
+  publishVersion: "1.0.1",
+  
+  previousVersion: "1.0",
+  previousPublishDate: "2021-02-18",
+  previousMaturity: "WV",
+  title: "NL GOV Assurance profile for OpenID Connect",
+  content: {"NLGovOpenID": "informative"},
+  editors:
+    [
+      {
+        name: "Remco Schaar",
+        company: "Logius",
+        companyURL: "https://github.com/Logius-standaarden",
+      },
+      {
+        name: "Frank van Es",
+        company: "Logius",
+        companyURL: "https://github.com/Logius-standaarden",
+      },
+      {
+        name: "Pieter Hering",
+        company: "Logius",
+        companyURL: "https://github.com/Logius-standaarden",
+      },
+      {
+        name: "Martin van der Plas",
+        company: "Logius",
+        companyURL: "https://github.com/Logius-standaarden",
+      }
+    ],
+  authors:
+    [
+      {
+        name: "Remco Schaar",
+        company: "Logius",
+        companyURL: "https://github.com/Logius-standaarden",
+      },
+      {
+        name: "Frank van Es",
+        company: "Logius",
+        companyURL: "https://github.com/Logius-standaarden",
+      },
+      {
+        name: "Joris Joosten",
+        company: "VZVZ",
+        companyURL: "https://www.vzvz.nl/",
+      },
+      {
+        name: "Jan Geert Koops",
+        company: "Dictu",
+        companyURL: "https://www.dictu.nl/",
+      }      
+    ],
+  github: "https://github.com/Logius-standaarden/OIDC-NLGOV/",
+
+
+  // Create PDF and link to file in header (optional):
+  alternateFormats: [
+      {
+          label: "pdf",
+          uri: "NLGov-OpenIDConnect-profile.pdf",
+      },
+  ],
+};
