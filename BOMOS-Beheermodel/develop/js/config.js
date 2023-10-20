a3b4d71ece750e495543c3a887f685db7109cb5e
GitHub Action <api@logius.nl>
2023-10-20 08:04:50 +0000

    new preview build

---
+++ BOMOS-Beheermodel/develop/js/config.js
@@ -0,0 +1,33 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "BOMOS-Beheermodel.pdf"
+      } ],
+  authors: [ { 
+        "mailto" : "bomos@logius.nl",
+        "name" : "Logius",
+        "url" : "https://logius.nl/standaarden"
+      } ],
+  editors: [ 
+      { 
+        "company" : "Logius",
+        "name" : "GÃ¼l IÅŸik",
+        "url" : "https://logius.nl/standaarden"
+      },
+      { 
+        "company" : "Logius",
+        "name" : "Edwin Wisse",
+        "url" : "https://logius.nl/standaarden"
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/BOMOS-Beheermodel",
+  nl_markdownCodeClass: "code",
+  previousPublishDate: "2022-06-01",
+  previousPublishVersion: "0.5",
+  pubDomain: "bomos",
+  publishDate: "2023-09-23",
+  publishVersion: "1.0",
+  shortName: "beheer",
+  specStatus: "WV",
+  specType: "HR"
+};
