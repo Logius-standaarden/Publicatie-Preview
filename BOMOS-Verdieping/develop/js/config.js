ed6cb21287d24d22051935f58d8af9ca70357d42
GitHub Action <api@logius.nl>
2023-11-13 08:52:47 +0000

    new preview build

---
+++ BOMOS-Verdieping/develop/js/config.js
@@ -0,0 +1,43 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "BOMOS-Verdieping.pdf"
+      } ],
+  authors: [ { 
+        "company" : "Universiteit Twente",
+        "companyURL" : "https://www.utwente.nl/",
+        "name" : "Erwin Folmer"
+      } ],
+  editors: [ 
+      { 
+        "company" : "Universiteit Twente",
+        "companyURL" : "https://www.utwente.nl/",
+        "name" : "Erwin Folmer"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://www.logius.nl/",
+        "name" : "Gül Işik"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://www.logius.nl/",
+        "name" : "Edwin Wisse"
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/BOMOS-Verdieping",
+  maxTocLevel: 3,
+  nl_markdownTableClass: "dkkvs",
+  previousMaturity: "DEF",
+  previousPublishVersion: "3.0.0",
+  pubDomain: "bomos",
+  publishDate: "2023-11-02",
+  publishVersion: "3.0.1",
+
+
+  shortName: "verdieping",
+  specStatus: "WV",
+  specType: "HR",
+
+  postProcess: [window.respecMermaid.createFigures],
+};
