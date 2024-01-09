3956b792eb8eb5d266e7eecc65abf1fc7a22da00
GitHub Action <api@logius.nl>
2024-01-09 16:26:06 +0000

    new preview build

---
+++ OIN-Stelsel/portaal/js/config.js
@@ -0,0 +1,27 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "OIN-Stelsel.pdf"
+      } ],
+  authors: [ { 
+        "company" : "Logius",
+        "companyURL" : "http://www.logius.nl/",
+        "name" : "Pieter Hering"
+      } ],
+  editors: [ { 
+        "company" : "Logius",
+        "companyURL" : "http://www.logius.nl/",
+        "name" : "Pieter Hering"
+      } ],
+  github: "https://github.com/Logius-standaarden/OIN-Stelsel",
+  nl_markdownCodeClass: "code",
+  nl_markdownTableClass: "dkkvs",
+  previousPublishVersion: "2.0.3",
+  previousPublishDate: "2020-03-31",
+  pubDomain: "dk",
+  publishDate: "2023-11-30",
+  publishVersion: "2.1.0",
+  shortName: "oin",
+  specStatus: "WV",
+  specType: "HR"
+};
