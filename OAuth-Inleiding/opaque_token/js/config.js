95afbf9a665471d0ed6408638e5ff54bef7731ac
GitHub Action <api@logius.nl>
2024-12-06 10:41:24 +0000

    new preview build

---
+++ OAuth-Inleiding/opaque_token/js/config.js
@@ -0,0 +1,40 @@
+var respecConfig = {
+  useLogo: true,
+  useLabel: true,
+  // license: "cc0",
+  specStatus: "WV",
+  specType: "PR",
+  pubDomain: "api",
+  shortName: "oauth-inleiding",
+  publishDate: "2024-11-16",
+  publishVersion: "0.1.0",
+  title: "Algemene Inleiding Open Authenticatie (OAuth)",
+  // previousPublishVersion: "(none)",
+  // content: {"ch01": "informative", "ch02": "", "mermaid": ""},
+  editors:
+    [
+      {
+        name: "Logius Standaarden",
+        company: "Logius",
+        companyURL: "https://github.com/Logius-standaarden",
+      }
+    ],
+  authors:
+    [
+      {
+        name: "Martin van der Plas",
+        company: "Logius",
+        companyURL: "https://github.com/Logius-standaarden",
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/OAuth-Inleiding",
+
+
+  // Create PDF and link to file in header (optional):
+  alternateFormats: [
+      {
+          label: "pdf",
+          uri: "OAuth-Inleiding.pdf",
+      },
+  ],
+};
