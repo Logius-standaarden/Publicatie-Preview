2612e348a9405f2b305b754f5b9fb45aad1aa3a2
GitHub Action <api@logius.nl>
2025-01-15 15:12:32 +0000

    new preview build

---
+++ Digikoppeling-Koppelvlakstandaard-ebMS2/script-remove/js/config.js
@@ -0,0 +1,25 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "Digikoppeling-Koppelvlakstandaard-ebMS2.pdf"
+      } ],
+  authors: [ { 
+        "name" : "Logius",
+        "url" : "https://logius.nl/standaarden"
+      } ],
+  editors: [ { 
+        "company" : "Logius",
+        "name" : "Logius",
+        "url" : "https://logius.nl/standaarden"
+      } ],
+  github: "https://github.com/Logius-standaarden/Digikoppeling-Koppelvlakstandaard-ebMS2",
+  maxTocLevel: 4,
+  previousPublishDate: "2022-04-11",
+  previousPublishVersion: "3.3.1",
+  pubDomain: "dk",
+  publishDate: "2024-05-31",
+  publishVersion: "3.3.2",
+  shortName: "ebms",
+  specStatus: "DEF",
+  specType: "ST"
+};
