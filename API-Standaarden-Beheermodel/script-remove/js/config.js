c826274f339a95663dad1cdf15b55747dfb42d79
GitHub Action <api@logius.nl>
2025-01-15 15:15:48 +0000

    new preview build

---
+++ API-Standaarden-Beheermodel/script-remove/js/config.js
@@ -0,0 +1,39 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "API-Standaarden-Beheermodel.pdf"
+      } ],
+  authors: [ { 
+        "mailto" : "Martin.plas@logius.nl",
+        "name" : "Martin van der Plas",
+        "company" : "Logius",
+        "companyURL" : "https://logius.nl/standaarden"
+      } ],
+  editors: [ { 
+        "mailto" : "api@logius.nl",
+        "name" : "Logius Afdeling standaarden"
+      }, 
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://logius.nl/standaarden",
+        "name" : "Alexander Green"
+      }, 
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://logius.nl/standaarden",
+        "name" : "Edwin Wisse"
+      }, 
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://logius.nl/standaarden",
+        "name" : "Peter Haasnoot"
+      } ],
+  github: "https://github.com/Logius-standaarden/API-Standaarden-Beheermodel",
+  pubDomain: "api",
+  publishDate: "2024-03-07",
+  publishVersion: "1.0",
+  previousPublishVersion: "1.0",
+  shortName: "beheermodel",
+  specStatus: "DEF",
+  specType: "HR"
+};
