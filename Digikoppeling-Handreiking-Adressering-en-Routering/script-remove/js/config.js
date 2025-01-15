1bdea39ace0220e516ac6d6dbe9d96a5f426292e
GitHub Action <api@logius.nl>
2025-01-15 14:58:13 +0000

    new preview build

---
+++ Digikoppeling-Handreiking-Adressering-en-Routering/script-remove/js/config.js
@@ -0,0 +1,25 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "Digikoppeling-Handreiking-Adressering-en-Routering.pdf"
+      } ],
+  authors: [ { 
+        "company" : "Logius",
+        "companyURL" : "http://www.logius.nl/",
+        "name" : "Logius"
+      } ],
+  editors: [ { 
+        "company" : "Logius",
+        "companyURL" : "http://www.logius.nl/",
+        "name" : "Logius"
+      } ],
+  github: "https://github.com/Logius-standaarden/Digikoppeling-Handreiking-Adressering-en-Routering",
+  nl_markdownCodeClass: "code",
+  previousPublishVersion: "1.0.1",
+  pubDomain: "dk",
+  publishDate: "2023-02-13",
+  publishVersion: "1.1.0",
+  shortName: "bpadres",
+  specStatus: "WV",
+  specType: "HR"
+};
