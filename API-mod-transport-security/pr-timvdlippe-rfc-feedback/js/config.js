4fd10b018d3be5789a4e18d49761cb8bc262e6c0
GitHub Action <api@logius.nl>
2025-01-27 10:51:09 +0000

    new preview build

---
+++ API-mod-transport-security/pr-timvdlippe-rfc-feedback/js/config.js
@@ -0,0 +1,41 @@
+let respecConfig = {
+    pubDomain: "api",
+    shortName: "mod-ts",
+	specType: "HR",
+	specStatus: "DEF",
+    publishDate: "2024-06-06",
+    publishVersion: "1.0.2",
+    previousPublishVersion: "1.0.1",
+    previousPublishDate: "2024-03-07",
+    editors: [ 
+      { 
+        "company" : "Geonovum",
+        "companyURL" : "https://www.geonovum.nl",
+        "name" : "Frank Terpstra"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://www.logius.nl",
+        "name" : "Alexander Green"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://www.logius.nl",
+        "name" : "Martin van der Plas"
+      }
+    ],
+    authors: [{
+        name: "Kennisplatform API's",
+        company: "Kennisplatform API's",
+        companyURL: "https://github.com/Geonovum/KP-APIs",
+    }],
+    github: "https://github.com/Logius-standaarden/API-mod-transport-security",
+    localBiblio: {
+        "NCSC.TLS": {
+            href: "https://www.ncsc.nl/documenten/publicaties/2021/januari/19/ict-beveiligingsrichtlijnen-voor-transport-layer-security-2.1",
+            title: "ICT-beveiligingsrichtlijnen voor Transport Layer Security (TLS)",
+            authors: ["NCSC"],
+            date: "Januari 2021"
+        }
+    }
+};
