0ef587bed3416ac4809a493a3748fb39968c6cbd
GitHub Action <api@logius.nl>
2025-02-03 18:11:13 +0000

    new preview build

---
+++ Digikoppeling-Koppelvlakstandaard-REST-API/RC2025-II/js/config.js
@@ -0,0 +1,32 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "Digikoppeling-Koppelvlakstandaard-REST-API.pdf"
+      } ],
+  authors: [ 
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://www.logius.nl/",
+        "name" : "Peter Haasnoot"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://www.logius.nl/",
+        "name" : "Pieter Hering"
+      }
+    ],
+  editors: [ {    
+        "company" : "Logius",  
+        "companyURL" : "https://www.logius.nl/",
+        "name" : "Standaardenbeheer"
+      } ],
+  github: "https://github.com/Logius-standaarden/Digikoppeling-Koppelvlakstandaard-REST-API",
+  previousPublishVersion: "2.0.1",
+  pubDomain: "dk",
+  //publishDate: "2025-01-30",
+  publishVersion: "3.0.0",
+  shortName: "restapi",
+  specStatus: "DEF",
+  specType: "ST",
+  postProcess: [window.respecMermaid.createFigures]
+};
