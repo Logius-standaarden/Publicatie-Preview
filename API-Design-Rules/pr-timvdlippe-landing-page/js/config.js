3c50d4e18e58ea47af4b398b4f850d50d7925fee
GitHub Action <api@logius.nl>
2025-02-11 10:02:13 +0000

    new preview build

---
+++ API-Design-Rules/pr-timvdlippe-landing-page/js/config.js
@@ -0,0 +1,60 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "API-Design-Rules.pdf"
+      } ],
+  authors: [ 
+      { 
+        "company" : "Het Kadaster",
+        "companyURL" : "https://www.kadaster.nl",
+        "name" : "Jasper Roes"
+      },
+      { 
+        "company" : "Het Kadaster",
+        "companyURL" : "https://www.kadaster.nl",
+        "name" : "Joost Farla"
+      }
+    ],
+  editors: [ 
+      { 
+        "company" : "Geonovum",
+        "companyURL" : "https://www.geonovum.nl",
+        "name" : "Frank Terpstra"
+      },
+      { 
+        "company" : "Geonovum",
+        "companyURL" : "https://www.geonovum.nl",
+        "name" : "Jan van Gelder"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://www.logius.nl",
+        "name" : "Alexander Green"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://www.logius.nl",
+        "name" : "Martin van der Plas"
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/API-Design-Rules",
+  pubDomain: "api",
+  publishDate: "2025-01-07",
+  publishVersion: "2.0.1",
+  previousPublishDate: "2024-03-07",
+  previousPublishVersion: "2.0.0",
+  shortName: "adr",
+  specStatus: "WV",
+  specType: "ST",
+  pluralize: true,
+
+  localBiblio: {
+    OData: {
+      authors: ["Michael Pizzo", "Ralf Handl", "Martin Zurmuehl"],
+      date: "23 April 2020",
+      href: "https://docs.oasis-open.org/odata/odata/v4.01/odata-v4.01-part2-url-conventions.html",
+      publisher: "OASIS Standard",
+      title: "OData Version 4.01. Part 2: URL Conventions",
+    },
+  },
+};
