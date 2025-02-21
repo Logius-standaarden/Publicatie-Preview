7eeee702e88596b10616e107164b3913a10f8b6d
GitHub Action <api@logius.nl>
2025-02-21 12:29:56 +0000

    new preview build

---
+++ Digikoppeling-Beheermodel/pr-timvdlippe-update-digikoppeling-overview/js/config.js
@@ -0,0 +1,31 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "Digikoppeling-Beheermodel.pdf"
+      } ],
+  authors: [ { 
+        "mailto" : "digikoppeling@logius.nl",
+        "name" : "Logius"
+      } ],
+  editors: [ 
+      { 
+        "company" : "Logius",
+        "name" : "Edwin Wisse",
+        "url" : "https://logius.nl/standaarden"
+      },
+      { 
+        "company" : "Logius",
+        "name" : "Peter Haasnoot",
+        "url" : "https://logius.nl/standaarden"
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/Digikoppeling-Beheermodel",
+  previousPublishVersion: "1.7",
+  previousPublishDate: "2022-06-01",
+  pubDomain: "dk",
+  publishDate: "2023-09-23",
+  publishVersion: "1.8",
+  shortName: "beheer",
+  specStatus: "WV",
+  specType: "WA"
+};
