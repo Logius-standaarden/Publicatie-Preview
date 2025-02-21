ac5ed648045d358b03ac22798e2f3d610f19efdd
GitHub Action <api@logius.nl>
2025-02-21 12:35:40 +0000

    new preview build

---
+++ Digikoppeling-Best-Practices-WUS/pr-timvdlippe-update-digikoppeling-overview/js/config.js
@@ -0,0 +1,31 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "Digikoppeling-Best-Practices-WUS.pdf"
+      } ],
+  authors: [ { 
+        "mailto" : "digikoppeling@logius.nl",
+        "name" : "Logius"
+      } ],
+  editors: [ 
+      { 
+        "company" : "Logius",
+        "name" : "Peter Haasnoot",
+        "url" : "https://logius.nl/standaarden"
+      },
+      { 
+        "company" : "Logius",
+        "name" : "Pieter Hering",
+        "url" : "https://logius.nl/standaarden"
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/Digikoppeling-Best-Practices-WUS",
+  previousPublishDate: "2022-04-11",
+  previousPublishVersion: "1.10.1",
+  pubDomain: "dk",
+  publishDate: "2023-06-01",
+  publishVersion: "1.10.2",
+  shortName: "bpwus",
+  specStatus: "WV",
+  specType: "BP"
+};
