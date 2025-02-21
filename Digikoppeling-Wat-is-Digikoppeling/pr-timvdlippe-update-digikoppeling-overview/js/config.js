95db99cb48ac42145e18295a8393a3242f7f491a
GitHub Action <api@logius.nl>
2025-02-21 12:44:19 +0000

    new preview build

---
+++ Digikoppeling-Wat-is-Digikoppeling/pr-timvdlippe-update-digikoppeling-overview/js/config.js
@@ -0,0 +1,32 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "Digikoppeling-Wat-is-Digikoppeling.pdf"
+      } ],
+  authors: [ { 
+        "mailto" : "digikoppeling@logius.nl",
+        "name" : "Logius",
+        "url" : "https://logius.nl/standaarden"
+      } ],
+  editors: [ 
+      { 
+        "company" : "Logius",
+        "name" : "Peter Haasnoot",
+        "url" : "https://logius.nl/standaarden"
+      },
+      { 
+        "company" : "Logius",
+        "name" : "Pieter Hering",
+        "url" : "https://logius.nl/standaarden"
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/Digikoppeling-Wat-is-Digikoppeling",
+  previousMaturity: "DEF",
+  pubDomain: "dk",
+  publishDate: "2022-04-11",
+  publishVersion: "1.1.2",
+  previousPublishVersion: "1.1.1",
+  shortName: "watisdk",
+  specStatus: "WV",
+  specType: "HR"
+};
