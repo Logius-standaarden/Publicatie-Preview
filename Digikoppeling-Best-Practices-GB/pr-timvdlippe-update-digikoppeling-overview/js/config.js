d4acc93601eb468313db04d0aba12c133feac62a
GitHub Action <api@logius.nl>
2025-02-21 12:33:48 +0000

    new preview build

---
+++ Digikoppeling-Best-Practices-GB/pr-timvdlippe-update-digikoppeling-overview/js/config.js
@@ -0,0 +1,36 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "Digikoppeling-Best-Practices-GB.pdf"
+      } ],
+  authors: [ { 
+        "name" : "Logius",
+        "url" : "https://logius.nl/standaarden"
+      } ],
+  editors: [ 
+      { 
+        "company" : "Logius",
+        "name" : "Peter Haasnoot",
+        "companyURL" : "https://logius.nl/standaarden"
+      },
+      { 
+        "company" : "Logius",
+        "name" : "Pieter Hering",
+        "companyURL" : "https://logius.nl/standaarden"
+      },
+      { 
+        "company" : "Logius",
+        "name" : "Martin van der Plas",
+        "companyURL" : "https://logius.nl/standaarden"
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/Digikoppeling-Best-Practices-GB",
+  previousPublishDate: "2017-10-01",
+  previousPublishVersion: "3.1.1",
+  pubDomain: "dk",
+  publishDate: "2023-05-16",
+  publishVersion: "3.2.0",
+  shortName: "bpgb",
+  specStatus: "WV",
+  specType: "BP"
+};
