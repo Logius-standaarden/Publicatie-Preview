27f29671a769626be247d5462fa0466ded9c5678
GitHub Action <api@logius.nl>
2025-03-07 15:13:06 +0000

    new preview build

---
+++ API-Design-Rules/pr-timvdlippe-spectral-linter-configuration/js/config.js
@@ -0,0 +1,50 @@
+var respecConfig = {
+  alternateFormats: [ { 
+        "label" : "pdf",
+        "uri" : "API-Design-Rules.pdf"
+      } ],
+  authors: [ 
+      { 
+        "company" : "Het Kadaster",
+        "companyURL" : "https://www.kadaster.nl",
+        "name" : "Jasper Roes"
+      },
+      { 
+        "company" : "Het Kadaster",
+        "companyURL" : "https://www.kadaster.nl",
+        "name" : "Joost Farla"
+      }
+    ],
+  editors: [ 
+      { 
+        "company" : "Geonovum",
+        "companyURL" : "https://www.geonovum.nl",
+        "name" : "Frank Terpstra"
+      },
+      { 
+        "company" : "Geonovum",
+        "companyURL" : "https://www.geonovum.nl",
+        "name" : "Jan van Gelder"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://www.logius.nl",
+        "name" : "Alexander Green"
+      },
+      { 
+        "company" : "Logius",
+        "companyURL" : "https://www.logius.nl",
+        "name" : "Martin van der Plas"
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/API-Design-Rules",
+  pubDomain: "api",
+  publishDate: "2025-01-07",
+  publishVersion: "2.0.1",
+  previousPublishDate: "2024-03-07",
+  previousPublishVersion: "2.0.0",
+  shortName: "adr",
+  specStatus: "WV",
+  specType: "ST",
+  pluralize: true,
+};
