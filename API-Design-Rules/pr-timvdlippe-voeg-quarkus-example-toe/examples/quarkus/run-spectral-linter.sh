03d74e7046e4b4fbaa1e8ee2492db425a8740540
GitHub Action <api@logius.nl>
2025-04-09 08:41:01 +0000

    new preview build

--- API-Design-Rules/pr-timvdlippe-voeg-quarkus-example-toe/examples/quarkus/run-spectral-linter.sh
+++ API-Design-Rules/pr-timvdlippe-voeg-quarkus-example-toe/examples/quarkus/run-spectral-linter.sh
@@ -3,6 +3,6 @@ CURRENT_DIRECTORY=$(dirname "$0")
 
 cd $CURRENT_DIRECTORY
 
-./mvnw package
+./mvnw --no-transfer-progress package
 
 spectral lint -r ../../linter/spectral.yml target/generated/openapi/openapi.json
