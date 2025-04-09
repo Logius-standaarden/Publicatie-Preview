f79bccc4dcb83c0c6b973b2f22f836cbff91dd3a
GitHub Action <api@logius.nl>
2025-04-09 11:32:18 +0000

    new preview build

---
+++ logboek-dataverwerkingen_Juridisch-beleidskader/pr-timvdlippe-respec-configuratie-opruimen/js/config.js
@@ -0,0 +1,73 @@
+var respecConfig = {
+  specStatus: "WV",
+  specType: "PR",
+  pubDomain: "logboek",
+  shortName: "juridisch",
+  publishDate: "2024-12-13",
+  publishVersion: "0.8.3",
+
+  edDraftURI: "https://logius-standaarden.github.io/logboek-dataverwerkingen_Juridisch-beleidskader/",
+
+  editors:
+    [
+      {
+        name: "Vedran Bilanovic",
+        company: "Ministerie van Binnenlandse Zaken en Koninkrijksrelaties",
+        companyURL: "https://www.rijksoverheid.nl/ministeries/ministerie-van-binnenlandse-zaken-en-koninkrijksrelaties",
+      },
+      {
+        name: "Eelco Hotting",
+        company: "Ministerie van Binnenlandse Zaken en Koninkrijksrelaties",
+        companyURL: "https://www.rijksoverheid.nl/ministeries/ministerie-van-binnenlandse-zaken-en-koninkrijksrelaties",
+      },
+      {
+        name: "Jeroen Mulder",
+        company: "Ministerie van Binnenlandse Zaken en Koninkrijksrelaties",
+        companyURL: "https://www.rijksoverheid.nl/ministeries/ministerie-van-binnenlandse-zaken-en-koninkrijksrelaties",
+      },
+      {
+        name: "Pieter Teekens",
+        company: "Ministerie van Binnelandse Zaken en Koninkrijksrelaties",
+        companyURL: "https://www.rijksoverheid.nl/ministeries/ministerie-van-binnenlandse-zaken-en-koninkrijksrelaties",
+      },
+      {
+        name: "Nil Barua",
+        company: "Logius",
+        companyURL: "https://logius.nl",
+      },
+      {
+        name: "Martin van der Plas",
+        company: "Logius",
+        companyURL: "https://logius.nl",
+      },
+      {
+        name: "Tim van der Lippe",
+        company: "Logius",
+        companyURL: "https://logius.nl",
+      }
+    ],
+  authors:
+    [
+      {
+        name: "Wouter Diephuis",
+        company: "Ministerie van Binnenlandse Zaken en Koninkrijksrelaties",
+        companyURL: "https://www.rijksoverheid.nl/ministeries/ministerie-van-binnenlandse-zaken-en-koninkrijksrelaties",
+      },
+      {
+        name: "Mirian van Ansem",
+        company: "Ministerie van Binnenlandse Zaken en Koninkrijksrelaties",
+        companyURL: "https://www.rijksoverheid.nl/ministeries/ministerie-van-binnenlandse-zaken-en-koninkrijksrelaties",
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/logboek-dataverwerkingen_Juridisch-beleidskader",
+
+
+  // Create PDF and link to file in header (optional):
+  alternateFormats: [
+      {
+          label: "pdf",
+          uri: "Juridisch_Beleidskader-Logboek_Dataverwerking.pdf",
+      },
+  ],
+  postProcess: [window.respecMermaid.createFigures]
+};
