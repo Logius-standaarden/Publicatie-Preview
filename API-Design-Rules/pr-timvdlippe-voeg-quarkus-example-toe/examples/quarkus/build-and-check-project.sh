4f36a2785d0c839f2163e484a6888aabe7b81e88
GitHub Action <api@logius.nl>
2025-04-15 12:36:06 +0000

    new preview build

--- API-Design-Rules/pr-timvdlippe-voeg-quarkus-example-toe/examples/quarkus/build-and-check-project.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-CURRENT_DIRECTORY=$(dirname "$0")
-
-cd $CURRENT_DIRECTORY
-
-./mvnw --no-transfer-progress package
-
-../run-spectral-linter.sh $(realpath target/generated/openapi/openapi.json)
