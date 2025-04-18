6441101796ab28fe23a2282ed9092895ec679bf3
GitHub Action <api@logius.nl>
2025-04-18 09:32:28 +0000

    new preview build

---
+++ fsc-core/choice-of-network/js/config.js
@@ -0,0 +1,90 @@
+let respecConfig = {
+    useLogo: true,
+    useLabel: true,
+    license: "cc-by",
+    specStatus: "DEF",
+    specType: "ST",
+    pubDomain: "fsc",
+    shortName: "core",
+    publishDate: "2024-12-12",
+    publishVersion: "1.0.0",
+
+    previousMaturity: "DEF",
+    editors:
+        [
+            {
+                name: "VNG Realisatie",
+                company: "VNG",
+                companyURL: "https://vng.nl/rubrieken/onderwerpen/standaarden",
+            },
+            {
+                name: "Logius",
+                company: "Logius",
+                companyURL: "https://github.com/Logius-standaarden",
+            }
+        ],
+    authors:
+        [
+            {
+                name: "Eelco Hotting",
+                company: "Hotting IT",
+                extras: [
+                    {
+                        name: "Email",
+                        href: "mailto:rfc@hotting.it",
+                    }
+                ]
+            },
+            {
+                name: "Ronald Koster",
+                company: "PhillyShell",
+                extras: [
+                    {
+                        name: "Email",
+                        href: "mailto:rfc@phillyshell.nl",
+                    }
+                ]
+            },
+            {
+                name: "Henk van Maanen",
+                company: "AceWorks",
+                extras: [
+                    {
+                        name: "Email",
+                        href: "mailto:henk.van.maanen@aceworks.nl",
+                    }
+                ]
+            },
+            {
+                name: "Niels Dequeker",
+                company: "ND Software",
+                extras: [
+                    {
+                        name: "Email",
+                        href: "mailto:niels@nd-software.be",
+                    }
+                ]
+            },
+            {
+                name: "Edward van Gelderen",
+                company: "vanG IT",
+                extras: [
+                    {
+                        name: "Email",
+                        href: "mailto:e.van.gelderen@vang.nl",
+                    }
+                ]
+            },
+            {
+                name: "Pim Gaemers",
+                company: "Apily",
+                extras: [
+                    {
+                        name: "Email",
+                        href: "mailto:pim.gaemers@apily.dev",
+                    }
+                ]
+            },
+        ],
+    github: "https://github.com/Logius-standaarden/fsc-core",
+};
