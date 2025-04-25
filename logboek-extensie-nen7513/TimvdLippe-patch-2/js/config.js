0fefec6bc4c2fb9286210510f095edb1392040fb
GitHub Action <api@logius.nl>
2025-04-25 08:12:48 +0000

    new preview build

---
+++ logboek-extensie-nen7513/TimvdLippe-patch-2/js/config.js
@@ -0,0 +1,40 @@
+let respecConfig = {
+  useLogo: true,
+  useLabel: true,
+  license: "cc-by",
+  specStatus: "WV",
+  specType: "HR",
+  pubDomain: "logboek",
+  shortName: "logboek-nen7513",
+  publishDate: "2025-03-31",
+  publishVersion: "0.0.1",
+
+  previousMaturity: "WV",
+  editors:
+    [
+      {
+        name: "Logius Standaarden",
+        company: "Logius",
+        companyURL: "https://logius.nl",
+      }
+    ],
+  authors:
+    [
+      {
+        name: "Logius Standaarden",
+        company: "Logius",
+        companyURL: "https://logius.nl",
+      }
+    ],
+  github: "https://github.com/Logius-standaarden/logboek-extensie-nen7513",
+
+  localBiblio: {
+    NEN7513: {
+      authors: ["Normcommissie Informatievoorziening in de zorg"],
+      date: "December 2024",
+      href: "https://www.nen.nl/nen-7513-2024-nl-329182",
+      publisher: "NEN",
+      title: "Medische informatica - Logging - Vastleggen van acties op persoonlijke gezondheidsinformatie"
+  },
+  }
+};
