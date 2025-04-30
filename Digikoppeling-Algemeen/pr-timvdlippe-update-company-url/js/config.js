9363c86581a390d7c3de7914485a2e82a5014d52
GitHub Action <api@logius.nl>
2025-04-30 08:08:22 +0000

    new preview build

---
+++ Digikoppeling-Algemeen/pr-timvdlippe-update-company-url/js/config.js
@@ -0,0 +1,38 @@
+let respecConfig = {
+  useLogo: true,
+  useLabel: true,
+  specStatus: "DEF",
+  specType: "BD",
+  pubDomain: "dk",
+  shortName: "roadmap",
+  publishDate: "2024-06-11",
+  publishVersion: "2024-2025",
+  previousPublishVersion: "2022-2023",
+  
+  editors:
+    [
+      {
+        name: "Logius Standaarden",
+        company: "Logius",
+        companyURL: "https://logius.nl",
+      }
+    ],
+  authors:
+    [
+      {
+        name: "Logius Standaarden",
+        company: "Logius",
+        companyURL: "https://logius.nl",
+      }
+    ],
+    github: "https://github.com/Logius-standaarden/Digikoppeling-Algemeen",
+
+
+  // Create PDF and link to file in header (optional):
+  alternateFormats: [
+      {
+          label: "pdf",
+          uri: "roadmap.pdf",
+      },
+  ],
+};
